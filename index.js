35f2a1c78783d3e96723554509f08c556b796e38
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-02-26 09:43:59 +0000

    added new settings to style config

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ module.exports = {
         "stylelint-order"
     ],
     rules: {
+        "string-quotes": null,
+        "block-no-empty": null,
+        "rule-empty-line-before": null,
+        "declaration-colon-space-after": null,
+        "function-parentheses-space-inside": null,
+        "color-named": null,
         "sh-waqar/declaration-use-variable": [["/color/", { ignoreValues: ["transparent", "inherit"] }]],
         "order/properties-alphabetical-order": true,
         "function-url-quotes": "always",
         "selector-class-pattern": null,
-        "no-eol-whitespace": null
+        "no-eol-whitespace": null,
+        "declaration-colon-space-before": null,
+        "at-rule-empty-line-before": null
     }
 };
